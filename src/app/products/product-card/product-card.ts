0155f9e8613c7a70c200f79390f8c24988355999
Diego Molver <molverdiego@gmail.com>
2025-07-26 00:22:43 -0300

    Iterar array de remeras para mostrarlas en la seccion

--- src/app/products/product-card/product-card.ts
+++ src/app/products/product-card/product-card.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProductsContainer } from '../products-container/products-container'; 
+
+// Interfaz para definir la estructura de un producto
+export interface Producto {
+  nombre: string;
+  descripcion: string;
+  descripcionDetallada: string;
+  precio: number;
+  imagen: string;
+  imagenesAdicionales: string[];
+  categoria: string;
+  talles: string[];
+}
 
 @Component({
   selector: 'app-product-card',
@@ -6,6 +19,4 @@ import { Component } from '@angular/core';
   templateUrl: './product-card.html',
   styleUrl: './product-card.css'
 })
-export class ProductCard {
-
-}
+export class ProductCard { @Input() producto!: Producto; }
